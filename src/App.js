ff2fe0303b4eef3d0dd8d93cc0420557389bec3e
Brock Brown <brockrbrown@gmail.com>
2022-08-17 23:14:20 -0400

    Added a better system for grabbing images from the filesystem, cleaning up, starting to look pretty good.

--- src/App.js
+++ src/App.js
@@ -3,31 +3,43 @@ import {Button, Alert} from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import logo from './logo.svg';
 import connectedGnome from './connected_gnome.jfif';
 import disconnectedGnome from './disconnected_gnome.jfif';
-import breakdancingGnome from './images/gnomes/breakdancing_gnome.jfif'
 import './App.css';
 import io from 'socket.io-client';
 
-const socket = io('localhost:3001');
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+  return images;
+}
 
+const getRandomProperty = (obj) => {
+  let keys = Object.keys(obj);
+  return obj[keys[Math.floor(Math.random() * keys.length)]];
+};
+
+const gnomeImages = importAll(require.context('./images/gnomes', false, /\.(jfif|png|jpe?g|svg)$/));
+const spinningGnome = getRandomProperty(gnomeImages);
+
+const socket = io('localhost:3001');
+const disconnectedGnomeMarkup = <p><span style = {{color:'red'}}>AUGH!</span> disconnected.</p>
+const connectedGnomeMarkup = <p><span style = {{color:'green'}}>connected ;)</span></p>
 function App() {
   const [isConnected, setIsConnected] = useState(socket.connected);
   const [lastMessage, setLastMessage] = useState(null);
   const [connectedGnomePicture, setConnectedGnomePicture] = useState(disconnectedGnome);
-  const [connectedGnomeText, setConnectedGnomeText] = useState(<p>Loading...</p>);
-
+  const [connectedGnomeText, setConnectedGnomeText] = useState(disconnectedGnomeMarkup);
   useEffect(() => {
     socket.on('connect', () => {
       setIsConnected(true);
       setConnectedGnomePicture(connectedGnome);
-      setConnectedGnomeText("connected ;)");
+      setConnectedGnomeText(connectedGnomeMarkup);
     });
     socket.on('disconnect', () => {
       setIsConnected(false);
-      setConnectedGnomeText(<p>AUGH! disconnected.</p>);
-      setConnectedGnomePicture(disconnectedGnome);
+      setConnectedGnomeText();
+      setConnectedGnomePicture(disconnectedGnomeMarkup);
     });
     socket.on('message', data => {
       setLastMessage(data);
@@ -42,16 +54,16 @@ function App() {
   const sendMessage = () => {
     socket.emit('hello!');
   }
-
+const thing = <p>test</p>;
   return (
     <Container style = {{textAlign:'center'}}>
       <Row>
-        <Alert variant = 'primary'>Alert!!! WEE WOO WEE WOO</Alert>
         <h1>Welcome to <span className = 'rainbow-text'>SPINTHEGNO.ME</span></h1>
       </Row>
       <Row>
         <Col className="text-center">
-          <img src={breakdancingGnome} className="Spinning-gnome" alt="logo"/>
+          {thing}
+          <img src={spinningGnome} className="Spinning-gnome" alt="logo"/>
         </Col>
       </Row>
       <Row>
