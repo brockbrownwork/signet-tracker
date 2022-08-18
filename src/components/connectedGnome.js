f70b6bd639aa418d1b5ef34e30656573cc36fb13
Brock Brown <brockrbrown@gmail.com>
2022-08-17 20:15:23 -0400

    More stuff

--- src/components/connectedGnome.js
+++ src/components/connectedGnome.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import connectedGnome from './connected_gnome.jfif';
-import disconnectedGnome from './disconnected_gnome.jfif';
 import './App.css';
 import io from 'socket.io-client';
 
@@ -10,16 +9,13 @@ const socket = io('localhost:3001');
 function App() {
   const [isConnected, setIsConnected] = useState(socket.connected);
   const [lastMessage, setLastMessage] = useState(null);
-  const [connectedGnomePicture, setConnectedGnomePicture] = useState('');
 
   useEffect(() => {
     socket.on('connect', () => {
       setIsConnected(true);
-      setConnectedGnomePicture(connectedGnome);
     });
     socket.on('disconnect', () => {
       setIsConnected(false);
-      setConnectedGnomePicture(disconnectedGnome);
     });
     socket.on('message', data => {
       setLastMessage(data);
@@ -38,9 +34,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={connectedGnome} className="App-logo" alt="logo" />
         <p>Connected: { '' + isConnected }</p>
-        <img src = {connectedGnomePicture} />
         <p>Last message: { lastMessage || '-' }</p>
         <button onClick={ sendMessage }>Say hello!</button>
         <p>
