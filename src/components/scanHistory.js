571222e055ec8b3825f36ac7edf061351e479d3e
Hopper <Glenn.Hopper@signetjewelers.com>
2022-08-31 13:31:02 -0400

    Scan history is formatted better

--- src/components/scanHistory.js
+++ src/components/scanHistory.js
@@ -4,12 +4,26 @@ import { v4 as uuid } from 'uuid';
 function ScanHistory(props) {
   let historyList = ''
   if (props.scanHistory) {
-    historyList = props.scanHistory.map(entry => <div key = {uuid()}>{entry.location}, {entry.date} </div>)
-    console.log(historyList);
+    historyList = props.scanHistory.map(
+      (entry) => {
+        let date = new Date(Date.parse(entry.date))
+        let time = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit'})
+        historyList =
+          <div key = {uuid()}>
+            {entry.location}, {date.toLocaleDateString()}, {time}
+            <p>
+              {entry.comments}
+            </p>
+          </div>
+        console.log("historyList:", historyList)
+        return <span>
+          {historyList}
+        </span>
+      })
   }
   return (
     <div>
-        {historyList != [] && historyList != ''? historyList: 'ack! no history'}
+        {historyList != [] && historyList != ''? [<strong>History of last scan:</strong>, ...historyList]: 'ack! no history'}
     </div>
   );
 }
